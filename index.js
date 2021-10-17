73212b2513eac17e4a06f5683c24f7a75b76ec8d
justinkunz <justin_kunz@yahoo.com>
2021-10-17 16:44:25 -0500

    added try/catch wrapper to handler

--- index.js
+++ index.js
@@ -14,20 +14,24 @@ const writeToFile = (fileName, data) => {
 
 // Handler function
 const init = () => {
-  const posts = csvConverter.parseCsv(fileHandler.readPostsFile());
+  try {
+    const posts = csvConverter.parseCsv(fileHandler.readPostsFile());
 
-  const { matches: topPosts, nonMatches: otherPosts } = new FilterSet(posts)
-    .addCondition(filterRules.privacy('public'))
-    .addCondition(filterRules.minimumComments(10))
-    .addCondition(filterRules.minimumViews(9000))
-    .addCondition(filterRules.maxPostLength(40))
-    .run(['id'], args.isDetailedMode); // Returns entire object if "--detailed" arg provided. Otherwise, returns only the id
+    const { matches: topPosts, nonMatches: otherPosts } = new FilterSet(posts)
+      .addCondition(filterRules.privacy('public'))
+      .addCondition(filterRules.minimumComments(10))
+      .addCondition(filterRules.minimumViews(9000))
+      .addCondition(filterRules.maxPostLength(40))
+      .run(['id'], args.isDetailedMode); // Returns entire object if "--detailed" arg provided. Otherwise, returns only the id
 
-  const dailyTopPosts = dailyPosts.findDailyTopPosts(posts, 'likes'); // <- tracking metric is "likes"
+    const dailyTopPosts = dailyPosts.findDailyTopPosts(posts, 'likes'); // <- tracking metric is "likes"
 
-  writeToFile(FILES.OUTPUT.TOP_POSTS, topPosts);
-  writeToFile(FILES.OUTPUT.OTHER_POSTS, otherPosts);
-  writeToFile(FILES.OUTPUT.DAILY_TOP_POSTS, dailyTopPosts);
+    writeToFile(FILES.OUTPUT.TOP_POSTS, topPosts);
+    writeToFile(FILES.OUTPUT.OTHER_POSTS, otherPosts);
+    writeToFile(FILES.OUTPUT.DAILY_TOP_POSTS, dailyTopPosts);
+  } catch (err) {
+    console.error('An error occurred:', err);
+  }
 };
 
 init();
